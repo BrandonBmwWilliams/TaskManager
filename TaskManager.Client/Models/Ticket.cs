44cf8b7cf69e46a9f708109a9b64d8dbf2141d74
Brandon Williams <brandonbmwwilliams@gmail.com>
2026-03-22 12:53:02 -0700

    Reapply "Added a home page and a dashboard for tickets"
    This reverts commit fdcb0a857b6b5a0eb6f002a49608710ea01ebbc1.

--- TaskManager.Client/Models/Ticket.cs
+++ TaskManager.Client/Models/Ticket.cs
@@ -1,12 +1,23 @@
-﻿namespace TaskManager.Client.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Client.Models;
 
 public class Ticket
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100, ErrorMessage = "Title can't exceed 100 characters")]
     public string Title { get; set; } = "";
+
+    [Required(ErrorMessage = "Description is required")]
     public string Description { get; set; } = "";
+
     public TicketPriority Priority { get; set; } = TicketPriority.Medium;
     public TicketStatus Status { get; set; } = TicketStatus.Open;
+
+    [Required(ErrorMessage = "Name is required")]
     public string SubmittedBy { get; set; } = "";
+
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
